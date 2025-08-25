6cb929fb55ad7bd69c2ad80d6cebd194836b7beb
Jake Roggenbuck <jakeroggenbuck2@gmail.com>
2025-08-24 17:35:43 -0700

    Add sentence split

--- python/betterwriter_test.py
+++ python/betterwriter_test.py
@@ -1,5 +1,18 @@
 import pytest
+from betterwriter import split_sentences
 
 
-def test_foo():
-	assert 1 == 1
+def test_split_sentences():
+    sentences = [
+        "This is a sentence 1. This is another sentence.",
+        "Hello there. How are you?"
+    ]
+
+    answers = [
+        ["This is a sentence 1.", "This is another sentence."],
+        ["Hello there.", "How are you?"]
+    ]
+
+    for answer, sentence in zip(answers, sentences):
+        print(split_sentences(sentence), answer)
+        assert split_sentences(sentence) == answer
